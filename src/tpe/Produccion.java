1c2ba1661d65474da8137215ced8dd783e019a23
mrmatias96 <matias.lapolla.1@gmail.com>
2021-10-08 12:11:07 -0300

    prueba2

--- src/tpe/Produccion.java
+++ src/tpe/Produccion.java
@@ -7,7 +7,7 @@ public class Produccion {
     private ArrayList<Musical> musicales;
     private ArrayList<Coach> jurado;
     private ArrayList<Participante> participantes;
-
+    private int sarasa;
     /*
 
     no se como funciona pero ya esta implementado
