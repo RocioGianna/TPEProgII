051c38697d4eef67e629ddf5219b6560f4f11cfc
mrmatias96 <matias.lapolla.1@gmail.com>
2021-10-08 12:03:01 -0300

    se agrego la clase produccion

---
+++ src/tpe/Produccion.java
@@ -0,0 +1,20 @@
+package tpe;
+
+import java.util.ArrayList;
+
+public class Produccion {
+
+    private ArrayList<Musical> musicales;
+    private ArrayList<Coach> jurado;
+    private ArrayList<Participante> participantes;
+
+    /*
+
+    no se como funciona pero ya esta implementado
+
+    public method(type any){
+        return any;
+    }
+    */
+
+}
