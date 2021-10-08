c90edabc1a75d80f5b042a9414ce675e2d8f25a5
Rocio <ginnaccini@gmail.com>
2021-10-08 11:17:58 -0300

    clase participante

---
+++ src/tpe/Participante.java
@@ -0,0 +1,57 @@
+package tpe;
+
+import java.util.ArrayList;
+
+public class Participante {
+	private String nombre;
+	private String apellido;
+	private int edad;
+	private ArrayList<String>generosPreferidos;
+	private ArrayList<String>idiomas;
+	private ArrayList<String>instrumentos;
+	
+	
+	public Participante(String nombre, String apellido, int edad) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+		this.generosPreferidos = new ArrayList<>();
+		this.idiomas = new ArrayList<>();
+		this.instrumentos = new ArrayList<>();
+	}
+	
+	
+	
+	//getters y setters
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public ArrayList<String> getGenerosPreferidos() {
+		return new ArrayList<String>(generosPreferidos);
+	}
+
+	public ArrayList<String> getIdiomas() {
+		return new ArrayList<String>(idiomas);
+	}
+
+	public ArrayList<String> getInstrumentos() {
+		return new ArrayList<String>(instrumentos);
+	}
+
+
+}
